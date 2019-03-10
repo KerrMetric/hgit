9b3991fe39837b5255e6ba60f4955b6a5ad30105
shiki <shiki.tak@gmail.com>
2019-03-10 14:41:52 +0900

    add runInteractiveProcess

--- hgit.hs
+++ hgit.hs
@@ -1,1 +1,6 @@
-main = putStrLn "Hello, World!"
+import System.Process
+import System.IO
+
+main = do
+    (_,out,_,_) <- runInteractiveProcess "git" ["help"] Nothing Nothing
+    hGetContents out >>= putStrLn  
