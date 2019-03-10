b5faf8b4a55d3b34b57af50ae338e95bce5c2317
Takumi Karibe <takumi.k.5610@gmail.com>
2019-03-10 16:57:59 +0900

    filtering string

--- hgit.hs
+++ hgit.hs
@@ -1,6 +1,12 @@
 import System.Process
 import System.IO
+import Data.List
 
 main = do
     (_,out,_,_) <- runInteractiveProcess "git" ["help"] Nothing Nothing
-    hGetContents out >>= putStrLn  
+    contents <- hGetContents out
+    mapM_ putStrLn (filterTarget (lines contents))
+
+-- clone を filter してみる
+filterTarget :: [String] -> [String]
+filterTarget = filter (\line -> "clone" `isInfixOf` line)
